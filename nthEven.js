ff0196d2211598c6304d8a03e0b7012dd8ba17c3
abhis <abhishekshrivastava78@gmail.com>
2021-03-22 15:56:20 +0530

    Initial commit

---
+++ nthEven.js
@@ -0,0 +1,14 @@
+// Return the Nth Even Number
+//
+// nthEven(1) //=> 0, the first even number is 0
+// nthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)
+//
+// nthEven(100) //=> 198
+// nthEven(1298734) //=> 2597466
+function nthEven(n){
+    return (n-1)*2;
+}
+console.log(nthEven(1));//0
+console.log(nthEven(3));//4
+console.log(nthEven(100));//198
+console.log(nthEven(1298734));//0
